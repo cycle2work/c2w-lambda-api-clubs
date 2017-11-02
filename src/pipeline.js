8cb2032da0a49a5400d3047e858bac902305e630
Manuel Serra <nitroin@hotmail.com>
2017-11-02 13:06:30 +0100

    [chore]: restore logs

--- src/pipeline.js
+++ src/pipeline.js
@@ -5,7 +5,7 @@ import { retrieveClubs, retrieveReports } from "./services/mongo-db";
 
 export default async function pipeline(event, context, callback) {
 
-    // log.debug({ event });
+    log.debug({ event });
 
     const { queryStringParameters: { month, year } } = event;
 
@@ -15,10 +15,10 @@ export default async function pipeline(event, context, callback) {
             month: month || moment.utc().format("MM"),
             year: year || moment.utc().year()
         };
-        // log.debug({ query });
+        log.debug({ query });
 
         const reports = await retrieveReports(query);
-        // log.debug({ reports });
+        log.debug({ reports });
 
         const clubs = await retrieveClubs();
         log.debug({ clubs });
@@ -26,7 +26,6 @@ export default async function pipeline(event, context, callback) {
         const response = clubs.map(club => {
             const clubReports = reports.filter(x => x.club.id === club.id);
             const distance = clubReports.reduce((state, report) => {
-                // log.debug({ report });
                 const total = report.distances.reduce((prev, current) => prev + current);
                 return state + total;
             }, 0);
@@ -35,6 +34,8 @@ export default async function pipeline(event, context, callback) {
                 distance
             };
         });
+
+        log.debug({ response });
 
         callback(null, {
             statusCode: 200,
